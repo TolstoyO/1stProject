b5ea60f48062c1adf3dbaccec85ed6e87360ef95
Olesya <olesya.tolstoy@gmail.com>
2023-12-27 16:43:32 -0600

    Currently logged in User

--- force-app/main/default/lwc/practice24Edit/practice24Edit.js
+++ force-app/main/default/lwc/practice24Edit/practice24Edit.js
@@ -1,4 +1,4 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 import OPP_OBJECT from "@salesforce/schema/Opportunity";
 import NAME_FIELD from "@salesforce/schema/Opportunity.Name";
 import STAGE_FIELD from "@salesforce/schema/Opportunity.StageName";
@@ -10,9 +10,16 @@ import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 
 export default class Practice24_edit extends LightningElement {
+     /// if you are planning to put this form on the record page
+     //you can even get the objectAPINAme directly from the record page without import 
+     //@api
+     //objectApiName
 
     objectApiName = OPP_OBJECT; 
-    recordId= '006Dn00000EjyeIIAR';
+    //recordId= '006Dn00000EjyeIIAR';
+    @api // this decoration is used to make this property public, so the parent component
+    // or the record page can inject the recordID to this property automatically  if we are on record page 
+    recordId;
 
     fields = [NAME_FIELD, STAGE_FIELD, CLOSE_DATE_FIELD, AMOUNT_FIELD]; 
 
