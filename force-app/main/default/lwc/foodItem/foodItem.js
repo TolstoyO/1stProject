ab3fade6c59ee2710f46f89eccb3ac10fb3bf9e2
Olesya <olesya.tolstoy@gmail.com>
2023-12-27 17:43:36 -0600

    food item and food stand

---
+++ force-app/main/default/lwc/foodItem/foodItem.js
@@ -0,0 +1,13 @@
+import { LightningElement, api } from "lwc";
+export default class FoodItem extends LightningElement {
+
+   // item={
+      //  foodName: 'Pizza',
+       // calorie: 2000
+  //  }
+
+@api
+item;
+
+
+}
